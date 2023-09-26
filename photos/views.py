b41a70bdec9fe80c1580a9b6ac5358cfafc6512e
Onyeogulu Tochukwu Rowland <49307590+onyeogulu@users.noreply.github.com>
2023-09-26 11:14:23 +0100

    Second Commit

--- photos/views.py
+++ photos/views.py
@@ -1,30 +1,72 @@
 from django.shortcuts import render, redirect
 from .models import Category, Photo
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.views.generic import (
     DeleteView
 )
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CustomeUserCreationForm
 
+def loginUser(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request,username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('gallery')
+            
+    return render(request, 'photos/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+def registerUser(request):
+    form = CustomeUserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.save()
+        user = authenticate(request,username=user.username, password=request.POST['password1'])
+        
+        if user is not None:
+            login(request, user)
+            return redirect('gallery')
+    
+    context = {'form':form}
+    return render(request, 'photos/register.html', context)
+        
 
 # Create your views here.
+@login_required(login_url='login')
 def gallery(request):
+    user = request.user
     category = request.GET.get('category')
     if category == None:
-        photos = Photo.objects.all()
+        photos = Photo.objects.filter(Category__user=user)
     else:
-        photos = Photo.objects.filter(Category__name=category)
+        photos = Photo.objects.filter(Category__name=category, Category__user=user)
         
-    categories = Category.objects.all()
+    categories = Category.objects.filter(user=user)
     context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html', context)
 
+@login_required(login_url='login')
 def viewPhoto(request, pk):
     photo = Photo.objects.get(id=pk)
     context = {'photo':photo}
     return render(request, 'photos/photo.html', context)
 
+@login_required(login_url='login')
 def addPhoto(request):
-    categories = Category.objects.all()
+    user = request.user
+    
+    categories = user.category_set.all()
     if request.method == 'POST':
         data = request.POST
         image = request.FILES.get('image')
@@ -46,7 +88,7 @@ def addPhoto(request):
     context = {'categories': categories}
     return render(request, 'photos/add.html', context)
 
-class deletePhoto(DeleteView):
+class deletePhoto(LoginRequiredMixin, DeleteView):
     model = Photo
     template_name = 'photos/photo_confirm_delete.html'
     context_object_name = 'photo'
